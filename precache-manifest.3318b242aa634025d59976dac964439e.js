2f217ce2b049db9a1cf2207fc030cb218616996d
mike56k <miha.is@mail.ru>
2020-09-13 14:05:08 +0300

    Updates

---
+++ precache-manifest.3318b242aa634025d59976dac964439e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8c120981d9a3217faadf8a0932f76b",
+    "url": "/Donations.VKMA/index.html"
+  },
+  {
+    "revision": "cbe5e1dd315911f0dcec",
+    "url": "/Donations.VKMA/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "f5c8889c97565703b994",
+    "url": "/Donations.VKMA/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "cbe5e1dd315911f0dcec",
+    "url": "/Donations.VKMA/static/js/2.f098130c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Donations.VKMA/static/js/2.f098130c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c8889c97565703b994",
+    "url": "/Donations.VKMA/static/js/main.6ca8e244.chunk.js"
+  },
+  {
+    "revision": "a26f62060c536de108a9",
+    "url": "/Donations.VKMA/static/js/runtime-main.3e71b2b3.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Donations.VKMA/static/media/persik.4e1ec840.png"
+  }
+]);
